a32f35f6467f44fd8bf83e8ee5e776d367937ead
rrcen@digitalgd.com.cn <rrcen@digitalgd.com.cn>
2025-04-30 16:15:39 +0800

    init

---
+++ apps/mobile/capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+    appId: 'com.example.vnts',
+    appName: 'nx-nestjs-vue',
+    webDir: '../../dist/apps/mobile',
+};
+
+export default config;
